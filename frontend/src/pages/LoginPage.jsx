3cf725828babb8776163c51ce8406d1eb773c99c
Manish chauhan <prohamker07@gmail.com>
2025-06-15 16:27:18 +0530

    first commit

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -18,11 +18,11 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="h-screen grid lg:grid-cols-2">
-      {/* Left Side - Form */}
+    <div className="h-screen grid lg:grid-cols-2 mt-7">
+   
       <div className="flex flex-col justify-center items-center p-6 sm:p-12">
         <div className="w-full max-w-md space-y-8">
-          {/* Logo */}
+       
           <div className="text-center mb-8">
             <div className="flex flex-col items-center gap-2 group">
               <div
